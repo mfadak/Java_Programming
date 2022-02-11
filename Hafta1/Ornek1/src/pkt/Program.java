cc2dff02e3da3c1da1e038ff0b3a3538c6d567c8
M. Fatih ADAK <62707529+mfadak@users.noreply.github.com>
2022-02-11 21:52:28 +0300

    Program.java

---
+++ Hafta1/Ornek1/src/pkt/Program.java
@@ -0,0 +1,11 @@
+package pkt;
+
+public class Program {
+	public static void main(String[] args) {
+		// Tek satır yorum
+		/*
+		      Çoklu satır yorumu
+		*/
+		System.out.println("Merhaba Dünya");
+	}
+}
