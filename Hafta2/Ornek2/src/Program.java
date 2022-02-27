4584f482fdf360dc65476dd78c91c2d02aef84fa
M. Fatih ADAK <62707529+mfadak@users.noreply.github.com>
2022-02-27 12:09:38 +0300

    Program.java

---
+++ Hafta2/Ornek2/src/Program.java
@@ -0,0 +1,13 @@
+public class IlkProje {
+    public static void main(String[] args) {
+        float x=10;
+        double a=10;
+        float y=3;
+        double b=3;
+        float z = x/y;
+        double c = a/b;
+            
+        System.out.println("float:"+z);
+        System.out.println("double:"+c);
+    }  
+}
