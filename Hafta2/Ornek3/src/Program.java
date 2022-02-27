986042ad7f4d981063f9dc63ce53d60a27664963
M. Fatih ADAK <62707529+mfadak@users.noreply.github.com>
2022-02-27 12:10:34 +0300

    Program.java

---
+++ Hafta2/Ornek3/src/Program.java
@@ -0,0 +1,8 @@
+public class IlkProje {
+    public static void main(String[] args) { 
+        final double yercekimi = 9.81;
+        double kuvvet,kutle=78;
+        kuvvet = kutle * yercekimi;
+        System.out.println(kuvvet);
+    }   
+}
